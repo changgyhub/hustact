b870876b2b622452c8bfdd512813cf60cb4bdb57
Gao Chang <cgao96@gmail.com>
2016-04-16 21:57:45 +0800

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,6 +1,7 @@
 var ox = new Array(1000);
 var oy = new Array(1000);
 var collideDis = 9.5;
+var lightScale = 100;
 
 var stageRatio = window.innerWidth/window.innerHeight;
 var windowx = stageRatio * 600;
@@ -10,45 +11,57 @@ var moveTox = windowx/2-1, moveToy = windowy/2-1, vx = 0, vy = 0;
 var exitx;
 var exity;
 
+var running = true;
+var dead = false;
+var sumscore = 0;
+var currentScore = 999;
 
 // ************ generate obstacles **************
 
 var boardx = Math.floor(windowx / collideDis / 4) + 2;  //each unit in board is 2 * collide Distance
 var boardy = Math.floor(windowy / collideDis / 4) + 2; 
-r = new Array(boardx);// = createArray(boardx, boardy);
+var goN, goE;
+generate();
+
+function generate(){
+    r = new Array(boardx);// = createArray(boardx, boardy);
   
-for(var i=0; i<boardx; i++)
-    r[i] = new Array(boardy);
+    for(var i=0; i<boardx; i++)
+        r[i] = new Array(boardy);
   
-for(var i=0; i<boardx; i++)
-    for(var j=0; j<boardy; j++)
-        r[i][j] = 0;
-for(var i=0; i<boardx; i++){
-    r[i][0] = -1;
-    r[i][boardy-1] = -1;
-}
-for(var i=0; i<boardy; i++){
-    r[0][i] = -1;
-    r[boardx-1][i] = -1;
-}
+    for(var i=0; i<boardx; i++)
+        for(var j=0; j<boardy; j++)
+            r[i][j] = 0;
+    for(var i=0; i<boardx; i++){
+        r[i][0] = -1;
+        r[i][boardy-1] = -1;
+    }
+    for(var i=0; i<boardy; i++){
+        r[0][i] = -1;
+        r[boardx-1][i] = -1;
+    }
+    ox = new Array(1000);
+    oy = new Array(1000);
+    goN = Math.floor((Math.random() * 3) - 1);
+    goE = 0;
+    if (goN == 0)
+        while (goE == 0)
+            goE = Math.floor((Math.random() * 3) - 1);
 
-var goN = Math.floor((Math.random() * 3) - 1);
-var goE = 0;
-if (goN == 0)
-    while (goE == 0)
-        goE = Math.floor((Math.random() * 3) - 1);
+    r[Math.floor(boardx/2)][Math.floor(boardy/2)] = 1;  
+    go(Math.floor(boardx/2), Math.floor(boardy/2), goE, goN);
 
-r[Math.floor(boardx/2)][Math.floor(boardy/2)] = 1;  
-go(Math.floor(boardx/2), Math.floor(boardy/2), goE, goN);
-
-for(var i=1; i<boardx-1; i++){
-    for(var j=1; j<boardy-1; j++){
-        if(r[i][j] == 0){
-            ox.push((i-1) * 4 * collideDis + 20);
-            oy.push((j-1) * 4 * collideDis + 20);
+    for(var i=1; i<boardx-1; i++){
+        for(var j=1; j<boardy-1; j++){
+            if(r[i][j] == 0){
+                ox.push((i-1) * 4 * collideDis + 20);
+                oy.push((j-1) * 4 * collideDis + 20);
+            }
         }
     }
 }
+
+
 
 function check(x, y, xD, yD){
     //check forward
@@ -153,7 +166,7 @@ function add_light(){
     light.blendMode = PIXI.BLEND_MODES.ADD;
     light.x = hero.x;
     light.y = hero.y;
-    light.drawCircle(0, 0, 100);
+    light.drawCircle(0, 0, lightScale);
     light.endFill();
     stage.addChild(light);
 }
@@ -203,7 +216,7 @@ function add_obstacles(){
     }
 }
 
-add_mask();
+//add_mask();
 add_verge();
 add_exit();
 add_hero();
@@ -211,26 +224,92 @@ add_hero();
 add_obstacles();
 stage.addChild(container);
 
-add_light();
-container.mask = light;
+//add_light();
+//container.mask = light;
 
 stage.interactive = true;
 stage.on('mousedown', onDown);
 stage.on('touchstart', onDown);
 
 function onDown (e) {
-    moveTox = e.data.getLocalPosition(stage).x;
-    moveToy = e.data.getLocalPosition(stage).y;
-    var ratio = (moveToy - hero.y) / (moveTox - hero.x);
-    var buffvx = Math.sqrt(5 / (1 + ratio * ratio));
-    var buffvy = Math.sqrt(5 - buffvx * buffvx);
-    if (moveToy - hero.y > 0) vy = buffvy;
-    else vy = -buffvy;
-    if (moveTox - hero.x > 0) vx = buffvx;
-    else vx = -buffvx;
+    if (running && !dead){
+        moveTox = e.data.getLocalPosition(stage).x;
+        moveToy = e.data.getLocalPosition(stage).y;
+        var ratio = (moveToy - hero.y) / (moveTox - hero.x);
+        var buffvx = Math.sqrt(5 / (1 + ratio * ratio));
+        var buffvy = Math.sqrt(5 - buffvx * buffvx);
+        if (moveToy - hero.y > 0) vy = buffvy;
+        else vy = -buffvy;
+        if (moveTox - hero.x > 0) vx = buffvx;
+        else vx = -buffvx;
+    } else if (!dead){
+        // TODO
+    } else if (dead){
+        var xpos = e.data.getLocalPosition(stage).x;
+        var ypos = e.data.getLocalPosition(stage).y;
+        if (windowx / xpos < 3 && windowx / xpos > 1.5 && windowy / ypos < 2 && windowy / ypos > 1.5){
+            running = true;
+            dead = false;
+            currentScore = 999;
+            lightScale = 100;
+            generate();
+
+            for (var i = 0; i < obstacles.length; i++){
+                obstacles[i].x = ox[i];
+                obstacles[i].y = oy[i];
+            }
+            exit.clear();
+            exit.lineStyle(40, 0x00ff00, 1);
+            if (!exitx|| exitx == windowx){  //left or right
+                exit.moveTo(exitx,exity-20);
+                exit.lineTo(exitx,exity+20);
+            } else {  // up or bottom
+                exit.moveTo(exitx-20, exity);
+                exit.lineTo(exitx+20,exity);
+            }
+
+            output.visible = false;
+            hero.x = windowx/2;
+            hero.y = windowy/2;
+            animate();
+        }
+    }
+    
 }
 
+var score = new PIXI.Text('',{
+        font : 'bold italic 36px Arial',
+        fill : '#FFFFFF',
+        stroke : '#4a1850',
+        strokeThickness : 5,
+        dropShadow : true,
+        dropShadowColor : '#000000',
+        dropShadowAngle : Math.PI / 6,
+        dropShadowDistance : 6,
+        wordWrap : true,
+        wordWrapWidth : 440
+    });
+score.x = 10;
+score.y = 10;
+stage.addChild(score);
 
+
+var output = new PIXI.Text('Game Over\n    Retry!',{
+        font : 'bold italic 36px Arial',
+        fill : '#F7EDCA',
+        stroke : '#4a1850',
+        strokeThickness : 5,
+        dropShadow : true,
+        dropShadowColor : '#000000',
+        dropShadowAngle : Math.PI / 6,
+        dropShadowDistance : 6,
+        wordWrap : true,
+        wordWrapWidth : 440
+    });
+output.x = windowx/2 - 90;
+output.y = windowy/2 - 30;
+output.visible = false;
+stage.addChild(output);
 
 // ************** start animation **************
 
@@ -239,8 +318,15 @@ animate();
 
 
 function animate() {
+    currentScore--;
+    score.text = '' + (currentScore);
 
-    for (var i = 0; i < ox.length; i++){
+    if (!currentScore){
+        gameover();
+        return;
+    }
+
+    for (var i = 0; i < obstacles.length; i++){
         obstacles[i].x += - Math.random() + Math.random();
         obstacles[i].y += - Math.random() + Math.random();
     }
@@ -258,35 +344,44 @@ function distance(bx, by){
 
 
 
-var output = new PIXI.Text('Game Over',{
-        font : 'bold italic 36px Arial',
-        fill : '#F7EDCA',
-        stroke : '#4a1850',
-        strokeThickness : 5,
-        dropShadow : true,
-        dropShadowColor : '#000000',
-        dropShadowAngle : Math.PI / 6,
-        dropShadowDistance : 6,
-        wordWrap : true,
-        wordWrapWidth : 440
-    });
-output.x = windowx/6;
-output.y = windowy/2;
 
 function checkCollide(){
     if (((!exitx || exitx == windowx) && Math.abs(hero.x-exitx) < 20 && Math.abs(hero.y-exity) < 20) || ((!exity || exity == windowy) && Math.abs(hero.y-exity) < 20 && Math.abs(hero.x-exitx) < 20)){
         output.text = 'You win';
         stage.addChild(output);
         renderer.render(stage);
+        running = false;
         return;
     }
-    for (var i = 0; i < ox.length; i++){
-        if (distance(obstacles[i].x, obstacles[i].y) < collideDis || hero.x < 15 || hero.x > windowx - 15 || hero.y < 15 || hero.y > windowy - 15){
-            stage.addChild(output);
-            renderer.render(stage);
+    for (var i = 0; i < obstacles.length; i++){
+        if (distance(obstacles[i].x, obstacles[i].y) < collideDis){
+            lightScale -= 20;
+
+
+            // !!!!!!!!!!!   Bug : cannot shrink lightScale   !!!!!!!!!!!
+
+
+            obstacles[i].x += - Math.random()*20 + Math.random()*20;
+            obstacles[i].y += - Math.random()*20 + Math.random()*20;
+            if (!lightScale){
+                gameover();
+                return;
+            }
+            break;
+        }
+        if (hero.x < 15 || hero.x > windowx - 15 || hero.y < 15 || hero.y > windowy - 15){
+            gameover();
             return;
         }
     }
     renderer.render(stage);
     requestAnimationFrame(animate);
+}
+
+
+function gameover(){
+    output.visible = true;
+    renderer.render(stage);
+    running = false;
+    dead = true;
 }
